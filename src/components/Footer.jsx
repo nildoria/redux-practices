5e6c716f7738a8be4f1b6c554fc7ce78a69f13b0
nildoria <sabbir.ahmed87@gmail.com>
2023-10-17 20:49:47 +0600

    filtesr state manage on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,81 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { priorityFilter, statusFilter } from '../redux/filters/actions';
+
+const numberOfTodos = (todos_number) => {
+  switch (todos_number) {
+    case 0:
+      return 'No task';
+    case 1:
+      return `${todos_number} task`;
+
+    default:
+      return `${todos_number} tasks`;
+  }
+};
+
 function Footer() {
+  const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+  const dispatch = useDispatch();
+  const todosRemaining = todos.filter((todo) => !todo.completed).length;
+  const { status, colors } = filters;
+
+  const handleStatusFilter = (status) => {
+    dispatch(statusFilter(status));
+  };
+
+  const handlePriorityFilter = (color) => {
+    if (colors.includes(color)) {
+      dispatch(priorityFilter(color, 'removed'));
+    } else {
+      dispatch(priorityFilter(color, 'added'));
+    }
+  };
+
   return (
     <div className="mt-4 flex justify-between text-xs text-gray-500">
-      <p>2 tasks left</p>
+      <p>{numberOfTodos(todosRemaining)} left</p>
       <ul className="flex space-x-1 items-center text-xs">
-        <li className="cursor-pointer font-bold">All</li>
+        <li
+          className={`cursor-pointer ${status === 'All' && 'font-bold'}`}
+          onClick={() => handleStatusFilter('All')}
+        >
+          All
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Incomplete</li>
+        <li
+          className={`cursor-pointer ${status === 'Incomplete' && 'font-bold'}`}
+          onClick={() => handleStatusFilter('Incomplete')}
+        >
+          Incomplete
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Complete</li>
+        <li
+          className={`cursor-pointer ${status === 'Complete' && 'font-bold'}`}
+          onClick={() => handleStatusFilter('Complete')}
+        >
+          Complete
+        </li>
         <li></li>
         <li></li>
-        <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
-        <li className="h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer"></li>
-        <li className="h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer"></li>
+        <li
+          className={`h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer ${
+            colors.includes('green') && 'bg-green-500'
+          }`}
+          onClick={() => handlePriorityFilter('green')}
+        ></li>
+        <li
+          className={`h-3 w-3 border-2 border-yellow-500 md:hover:bg-yellow-500 rounded-full cursor-pointer ${
+            colors.includes('yellow') && 'bg-yellow-500'
+          }`}
+          onClick={() => handlePriorityFilter('yellow')}
+        ></li>
+        <li
+          className={`h-3 w-3 border-2 border-red-500 md:hover:bg-red-500 rounded-full cursor-pointer ${
+            colors.includes('red') && 'bg-red-500'
+          }`}
+          onClick={() => handlePriorityFilter('red')}
+        ></li>
       </ul>
     </div>
   );
