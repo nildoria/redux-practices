3c18d6c1d4d2a125df13fed3fe2beb96a362d3d2
nildoria <sabbir.ahmed87@gmail.com>
2023-10-17 21:18:07 +0600

    filtering todo list on ui

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,12 +3,38 @@ import Todo from './Todo';
 
 function TodoList() {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+
+  const filterByStatus = (todo) => {
+    const { status } = filters;
+    switch (status) {
+      case 'Complete':
+        return todo.completed;
+
+      case 'Incomplete':
+        return !todo.completed;
+
+      default:
+        return true;
+    }
+  };
+
+  const filterByPriority = (todo) => {
+    const { colors } = filters;
+    if (colors.length > 0) {
+      return colors.includes(todo?.color);
+    }
+    return true;
+  };
 
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {todos.map((todo) => (
-        <Todo todo={todo} key={todo.id} />
-      ))}
+      {todos
+        .filter(filterByStatus)
+        .filter(filterByPriority)
+        .map((todo) => (
+          <Todo todo={todo} key={todo.id} />
+        ))}
     </div>
   );
 }
